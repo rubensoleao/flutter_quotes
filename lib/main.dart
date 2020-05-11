01034908233c39108a52a9b34921df6b33a91fc9
Rubens Leão <rubensoleao@gmail.com>
2020-05-11 04:16:36 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() => runApp(MaterialApp(
+    home: QuoteList(),
+  )
+);
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  
+  List<Quote> quotes = [
+    Quote(text:"Melhor 1 passáro na mão do que 2 na mão", author:"Barack Obama"),
+    Quote(text:"Não tenho nada a declarar, apenas que você é lindo", author:"sua avó" ),
+    Quote(text: "Ei. Olá marcos!", author:"Tom Wiseau"),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+      )
+    );
+  }
+}
+
+
+
